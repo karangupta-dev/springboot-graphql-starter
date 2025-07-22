7d35e34ccb8ea60e754d7ea36f0ee229646ed74c
Karan Gupta <karangupta.dev@gmail.com>
2025-07-21 20:06:59 -0500

    Enable BookResolver and comment out BookQueryResolver
    Uncomments and activates the BookResolver class with its query and mutation mappings, while commenting out the BookQueryResolver. This likely transitions GraphQL query handling from BookQueryResolver to BookResolver, possibly to use the newer @Controller and @QueryMapping/@MutationMapping annotations.

--- src/main/java/com/example/graphql/resolver/BookQueryResolver.java
+++ src/main/java/com/example/graphql/resolver/BookQueryResolver.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
-public class BookQueryResolver implements GraphQLQueryResolver {
-    private final BookRepository bookRepository;
-
-    public BookQueryResolver(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-    public List<Book> allBooks() {
-        return bookRepository.findAll();
-    }
-}
+//@Component
+//public class BookQueryResolver implements GraphQLQueryResolver {
+//    private final BookRepository bookRepository;
+//
+//    public BookQueryResolver(BookRepository bookRepository) {
+//        this.bookRepository = bookRepository;
+//    }
+//
+//    public List<Book> allBooks() {
+//        return bookRepository.findAll();
+//    }
+//}
