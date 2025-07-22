2e2b0c7d4d872f1e2e76d8859d788d52729b99db
Karan Gupta <karangupta.dev@gmail.com>
2025-07-21 20:06:00 -0500

    Comment out HelloController and BookResolver classes
    The HelloController and BookResolver classes have been fully commented out, effectively disabling their functionality. This may be for debugging, refactoring, or to temporarily remove these endpoints from the application.

--- src/main/java/com/example/graphql/controller/HelloController.java
+++ src/main/java/com/example/graphql/controller/HelloController.java
@@ -3,11 +3,11 @@ package com.example.graphql.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, GraphQL!";
-    }
-}
+//@RestController
+//public class HelloController {
+//
+//    @GetMapping("/")
+//    public String hello() {
+//        return "Hello, GraphQL!";
+//    }
+//}
--- src/main/java/com/example/graphql/resolver/BookResolver.java
+++ src/main/java/com/example/graphql/resolver/BookResolver.java
@@ -10,35 +10,35 @@ import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@Controller
-public class BookResolver {
-
-    private final BookService bookService;
-
-    public BookResolver(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @QueryMapping
-    public List<Book> allBooks() {
-        return bookService.getAllBooks();
-    }
-
-    @QueryMapping
-    public Book bookById(@Argument String id) {
-        return bookService.getBookById(id);
-    }
-
-    @MutationMapping
-    public Book addBook(@Argument String id,
-                        @Argument String title,
-                        @Argument String author,
-                        @Argument double price) {
-        Book book = new Book();
-        book.setId(id);
-        book.setTitle(title);
-        book.setAuthor(author);
-        book.setPrice(price);
-        return bookService.addBook(book);
-    }
-}
+//@Controller
+//public class BookResolver {
+//
+//    private final BookService bookService;
+//
+//    public BookResolver(BookService bookService) {
+//        this.bookService = bookService;
+//    }
+//
+//    @QueryMapping
+//    public List<Book> allBooks() {
+//        return bookService.getAllBooks();
+//    }
+//
+//    @QueryMapping
+//    public Book bookById(@Argument String id) {
+//        return bookService.getBookById(id);
+//    }
+//
+//    @MutationMapping
+//    public Book addBook(@Argument String id,
+//                        @Argument String title,
+//                        @Argument String author,
+//                        @Argument double price) {
+//        Book book = new Book();
+//        book.setId(id);
+//        book.setTitle(title);
+//        book.setAuthor(author);
+//        book.setPrice(price);
+//        return bookService.addBook(book);
+//    }
+//}
