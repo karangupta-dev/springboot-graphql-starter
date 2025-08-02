54e496aabb8b024dfd54d7a9862014640a29a0f9
Karan Gupta <karangupta.dev@gmail.com>
2025-08-02 18:44:13 -0500

    Add Author entity and update GraphQL schema
    Introduced the Author entity and repository for author data management. Updated the GraphQL schema to support authors, including new types, queries, and mutations. Removed the unused BookQueryResolver.

---
+++ src/main/java/com/example/graphql/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.example.graphql.repository;
+
+import com.example.graphql.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, String> {}
